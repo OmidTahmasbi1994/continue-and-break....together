648a25c808765676522621548d0a003531200a13
Omid Tahmasbi <91671463+OmidTahmasbi1994@users.noreply.github.com>
2021-12-30 15:13:42 +0330

    Add files via upload

---
+++ continue and break....together.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main()
+
+{
+	int n= 1;
+	char c;
+	
+	for ( ; ; n++)
+	
+		{
+			cout<<"loop : "<<n<<endl;
+			cout<<"continue ? <y | n> ";
+			
+			cin>>c;
+			
+			if ( c=='y') continue;
+			
+			break;
+		}
+
+	cout<<"Total Of loops: "<<n<<endl;
+}
